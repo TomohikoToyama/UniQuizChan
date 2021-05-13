1e528fd29f0dc2158e318aa8c0cee7f46ddc5483
Tomohiko <tomotoyaya@gmail.com>
2021-05-13 19:04:55 +0900

    Add model.

---
+++ UniQuizChan/Assets/_Contens/Number001_MVRPettern/Answer/Scripts/CounterModel_A.cs
@@ -0,0 +1,23 @@
+﻿using UniRx;
+
+public class CounterModel_A
+{
+    public  IReactiveProperty<int> Counter => countNumber;
+    private ReactiveProperty<int> countNumber = new ReactiveProperty<int>();
+    
+    /// <summary>
+    /// Add count value.
+    /// </summary>
+    public void AddCount()
+    {
+        countNumber.Value += 1;
+    }
+
+    /// <summary>
+    /// Reduce count value.
+    /// </summary>
+    public void ReduceCount()
+    {
+        countNumber.Value -= 1;
+    }
+}
