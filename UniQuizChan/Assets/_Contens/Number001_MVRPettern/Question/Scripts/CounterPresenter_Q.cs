487a0062aed22e1b932aae0e00f5d9703a50be88
Tomohiko <tomotoyaya@gmail.com>
2021-05-14 17:18:33 +0900

    番号001の設問用シーンとスクリプト追加

---
+++ UniQuizChan/Assets/_Contens/Number001_MVRPettern/Question/Scripts/CounterPresenter_Q.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class CounterPresenter_Q : MonoBehaviour
+{
+    private CounterModel_A model;
+    [SerializeField] private CounterView_Q  view;
+    // Start is called before the first frame update
+    void Start()
+    {
+        model = new CounterModel_A();
+        // Nullチェック
+        if (null == view) view = FindObjectOfType<CounterView_Q>();
+
+        // viewを参照 
+        view
+            // AddObservableが発火した時
+            .AddObservable()
+            // 購読してmodelのAddCountを実行する
+            .Subscribe(_ => model.AddCount())
+            .AddTo(this);
+        
+        // viewを参照 .
+        view
+            // ReduceObservableが発火した時.
+            .ReduceObservable()
+            // 購読してmodelのReduceCountを実行する
+            .Subscribe(_ => model.ReduceCount())
+            .AddTo(this);
+
+        //  modelを参照する
+        model
+            // Counterの値に変化があった時
+            .Counter
+            // modelのCounterの値を引数にviewのDisplayCountを実行する
+            .Subscribe(_ => view.DisplayCount(model.Counter.Value))
+            .AddTo(this);
+    }
+
+}
