6b7342869873272d66a0f5f0118b034d5003f9df
Tomohiko <tomotoyaya@gmail.com>
2021-05-14 13:38:47 +0900

    Added the script for damage display.

---
+++ UniQuizChan/Assets/_Contens/Number003_SlashiingWithMouse/Answer/Scripts/DamageView.cs
@@ -0,0 +1,35 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageView_A : MonoBehaviour
+{
+    [SerializeField] private Text damageLog;
+    [SerializeReference] private MouseSlash _slash;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Browse to the _slash. 
+        _slash
+            // When fired Damage.    
+            .Damage
+            // Subscribe and excuete DisplayDamage.
+            .Subscribe(  point =>
+            {
+                DisplayDamage(point);
+            })
+            .AddTo(this);
+
+    }
+
+    /// <summary>
+    /// Display damage value
+    /// </summary>
+    /// <param name="point"></param>
+    private void DisplayDamage(int point)
+    {
+        damageLog.text = point + " Damage!!";
+    }
+    
+}
