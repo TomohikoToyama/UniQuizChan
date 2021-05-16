7cbdaa43fcc481330b899297dc0487373a165699
Tomohiko <tomotoyaya@gmail.com>
2021-05-16 23:43:30 +0900

    カウントダウンタイマー追加

---
+++ UniQuizChan/Assets/_Contens/Number002_Countdown/Answer/Scripts/CountdownTimer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+
+public class CountdownTimer : MonoBehaviour
+{
+    private int limitTime = 60;
+    private TimerData  _timerData;
+    private TimerView  _timerView;
+    private TimerSound _timerSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _timerData = new TimerData(0);
+        _timerView = FindObjectOfType<TimerView>().GetComponent<TimerView>();
+        _timerSound = GetComponent<TimerSound>();
+        
+        
+        _timerData
+            .ElapsedTime
+            .Where( y => _timerView.IsStart.Value)
+            .Subscribe( _ =>{
+                _timerSound.PlaySE(); 
+            })
+            .AddTo(this);
+         
+
+        Observable
+            .Interval(System.TimeSpan.FromSeconds(1)) //0秒後から1秒間隔で実行
+            .Where( y => _timerView.IsStart.Value)
+            .Subscribe(x =>
+            {
+                _timerData.ChangeTime();
+            });
+
+    }
+
+   
+}
---
+++ UniQuizChan/Assets/_Contens/Number002_Countdown/Answer/Scripts/TimerView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerView : MonoBehaviour
+{
+
+    [SerializeField] private Button startBtn;
+    [SerializeField] private Text countText;
+    public IReactiveProperty<bool> IsStart => IsStart;
+    ReactiveProperty<bool> isStart = new ReactiveProperty<bool>();
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (null == countText)
+            countText = GetComponent<Text>();
+
+        startBtn
+            .OnClickAsObservable()
+            .Where( x => !isStart.Value)
+            .Subscribe(_ =>
+            {
+                isStart.Value = true;
+                startBtn.gameObject.SetActive(false);
+            })
+            .AddTo(this);
+    }
+
+    public void DisplayTimer(int num)
+    {
+        countText.text = $"Rest : {num} sec";
+    }
+}
