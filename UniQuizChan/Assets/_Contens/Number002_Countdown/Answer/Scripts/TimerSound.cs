4d3512e351712ff7c17acd27c42f21c6b13524d3
Tomohiko <tomotoyaya@gmail.com>
2021-05-16 23:31:25 +0900

    タイマーの効果音処理追加

---
+++ UniQuizChan/Assets/_Contens/Number002_Countdown/Answer/Scripts/TimerSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerSound : MonoBehaviour
+{
+    [SerializeField] private AudioClip _clip;
+    private AudioSource _source;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if( null == _source)
+            _source = gameObject.AddComponent<AudioSource>();
+        _source.playOnAwake = false;
+        _source.clip = _clip;
+    }
+
+    public void PlaySE()
+    {
+        _source.Play();
+    }
+    
+}
