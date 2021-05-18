d3ec46c4b56537a2ca1769ab26ca30d469d3174f
Tomohiko <tomotoyaya@gmail.com>
2021-05-18 15:24:37 +0900

    時間に関する処理修正

--- UniQuizChan/Assets/_Contens/Number002_Countdown/Answer/Scripts/TimerData.cs
+++ UniQuizChan/Assets/_Contens/Number002_Countdown/Answer/Scripts/TimerData.cs
@@ -4,7 +4,9 @@ public class TimerData
 {
     public IReactiveProperty<int> ElapsedTime => _elapsedTime;
     private ReactiveProperty<int> _elapsedTime = new ReactiveProperty<int>();
-
+    public IReactiveProperty<bool> IsStart => isStart;
+    ReactiveProperty<bool> isStart = new ReactiveProperty<bool>();
+    
     public TimerData(int num)
     {
         _elapsedTime.Value = num;
@@ -12,5 +14,10 @@ public class TimerData
     public void ChangeTime()
     {
         _elapsedTime.Value += 1;
+    }
+
+    public void TimerStart(bool flg)
+    {
+        isStart.Value = flg;
     }
 }
