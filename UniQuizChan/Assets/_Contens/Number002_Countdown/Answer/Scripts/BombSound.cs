72d1f75983497c44ebb41c9463cdd4103a69e693
Tomohiko <tomotoyaya@gmail.com>
2021-05-18 15:24:17 +0900

    爆弾に関する処理追加

---
+++ UniQuizChan/Assets/_Contens/Number002_Countdown/Answer/Scripts/BombSound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BombSound : MonoBehaviour
+{
+    [SerializeField] private AudioSource _source; 
+    // fuseClip = 0, exposionCLip = 1
+   public enum ClipName
+    {
+        fuseClip = 0, 
+        exposionCLip = 1
+    }
+    [SerializeField] private AudioClip[] _clips = new AudioClip[2];
+    
+    public void PlaySE(int num)
+    {
+        _source.clip = _clips[num];
+        _source.Play();
+    }
+}
---
+++ UniQuizChan/Assets/_Contens/Number002_Countdown/Answer/Scripts/BombView.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BombView : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+
+
+    public void ChangeFuse(int num)
+    {
+        _slider.value = num;
+    }
+    
+    
+}
